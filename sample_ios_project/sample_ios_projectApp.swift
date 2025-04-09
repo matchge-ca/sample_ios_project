16330839ea9fdeba335144d251019563be7f8102
Hua Lu <hualu@gmail.com>
2025-04-09 10:31:26 -0400

    Initial Commit

---
+++ sample_ios_project/sample_ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_ios_projectApp.swift
+//  sample_ios_project
+//
+//  Created by Hua Lu on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct sample_ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
